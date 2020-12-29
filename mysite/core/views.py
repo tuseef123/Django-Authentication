9bb2cbda00d32398d3417de324bcf827ba981b1f
tuseef123 <tossefashraf.292791@gmail.com>
2020-12-29 15:50:11 +0500

    Login,Logout and password change is done password reset is remaning

---
+++ mysite/core/views.py
@@ -0,0 +1,17 @@
+from django.shortcuts import render,redirect
+
+from django.contrib.auth.models import User
+from django.contrib.auth.forms import UserCreationForm
+# Create your views here.
+def home(request):
+    count = User.objects.count()
+    return render(request,'home.html',{'count':count})
+def signup(request):
+    if request.method == 'POST':
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('home')
+    else:
+        form = UserCreationForm()
+    return render(request,'registration/signup.html',{'form':form})
